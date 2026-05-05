f924bc49574dc806d96b0e688063dbc70ceebc4c
serJ8o-bin <serj8o@tutamail.com>
2026-05-05 12:29:11 +0200

    Add files via upload

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -137,8 +137,73 @@
 
 
   # ly config
-  environment.etc."ly/config.ini".source = lib.mkForce ./ly-config.ini;
+  services.displayManager.ly.enable = true;
 
+  environment.etc."ly/config.ini".text = ''
+  allow_empty_password = true
+  animation = doom
+  animation_frame_delay = 4
+  animation_timeout_sec = 0
+  asterisk = *
+  auth_fails = 10
+  bigclock = en
+  bigclock_12hr = false
+  bigclock_seconds = false
+  blank_box = true
+  border_fg = 0x00FFFFFF
+  box_title = null
+  clear_password = false
+  clock = null
+  cmatrix_fg = 0x00FFFFFF
+  cmatrix_head_col = 0x01FFFFFF
+  cmatrix_min_codepoint = 0x21
+  cmatrix_max_codepoint = 0x7B
+  colormix_col1 = 0x00222222
+  colormix_col2 = 0x00808080
+  colormix_col3 = 0x20CCCCCC
+  default_input = login
+  doom_fire_height = 6
+  doom_fire_spread = 2
+  doom_top_color = 0x00222222
+  doom_middle_color = 0x00888888
+  doom_bottom_color = 0x00FFFFFF
+  edge_margin = 0
+  error_bg = 0x00000000
+  error_fg = 0x01FF0000
+  fg = 0x00FFFFFF
+  bg = 0x00000000
+  full_color = true
+  hide_borders = false
+  hide_key_hints = false
+  hide_keyboard_locks = false
+  hide_version_string = false
+  input_len = 34
+  lang = en
+  margin_box_h = 2
+  margin_box_v = 1
+  numlock = false
+  restart_key = F2
+  save = true
+  service_name = ly
+  session_log = .local/state/ly-session.log
+  shell = true
+  show_password_key = F7
+  show_tty = false
+  shutdown_key = F1
+  text_in_center = false
+  vi_mode = false
+  vi_default_mode = normal
+  xinitrc = ~/.xinitrc
+
+  brightness_down_cmd = ${pkgs.brightnessctl}/bin/brightnessctl -q -n s 10%-
+  brightness_up_cmd = ${pkgs.brightnessctl}/bin/brightnessctl -q -n s +10%
+  brightness_down_key = F5
+  brightness_up_key = F6
+  path = /run/current-system/sw/bin
+  restart_cmd = /run/current-system/systemd/bin/systemctl reboot
+  shutdown_cmd = /run/current-system/systemd/bin/systemctl poweroff
+  tty = 1
+  '';
 
   # fish
   programs.fish.enable = true;
